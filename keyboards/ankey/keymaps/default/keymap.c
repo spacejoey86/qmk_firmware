2798eaa229658fc9c1f5480695c1e52f9f63ea2a
Dylan Gardham <message4dylan@gmail.com>
2024-05-10 19:54:01 +0100

    right half test

---
+++ keyboards/ankey/keymaps/default/keymap.c
@@ -0,0 +1,27 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *           ┌───┐                   ┌───┐
+      *           │GUI├───┐           ┌───┤Alt│
+      *           └───┤Bsp├───┐   ┌───┤Ent├───┘
+      *               └───┤   │   │   ├───┘
+      *                   └───┘   └───┘
+      */
+    [0] = LAYOUT_split_3x5_3(
+                       KC_DOT,  KC_M,    KC_COMM, KC_N,    KC_SLSH,
+                       KC_BACKSPACE,    KC_K,    KC_J,    KC_L,    KC_H,
+                       KC_SCLN, KC_ENTER,    KC_I,    KC_U,    KC_O,
+              KC_Y,  KC_P,  KC_LEFT_SHIFT
+    )
+};
